5ba88c0195959825771545a2da2e9d588141f6a4
Kayl Eubanks <kayleubanks@gmail.com>
2018-04-20 09:55:37 -0700

    Code for first test, currently failing

--- spec/age_spec.js
+++ spec/age_spec.js
@@ -1,1 +1,15 @@
 import {Age} from "./../src/age.js";
+
+describe('Conversion of Earth age to other relative ages', function() {
+
+  it('should convert a provided age(in years) to seconds', function(){
+    const userAge = 24;
+    const ageCaller = new Age();
+    expect(ageCaller.secondConverter(userAge)).toEqual(757366848);
+  });
+
+});
+
+
+// const userAge = new Date(94, 3, 20);
+// const currentDate = new Date(2018, 3, 20);
