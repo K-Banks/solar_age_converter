c934fc5ea55a1b404b4b9710baafa38cefe47453
Kayl Eubanks <kayleubanks@gmail.com>
2018-04-20 10:19:41 -0700

    Write functionality for spec test 2, now passing

--- src/age.js
+++ src/age.js
@@ -6,6 +6,13 @@ class Age {
     return ageInSeconds;
   }
 
+  dateDifference(date1, date2) {
+    const date1FixedDifference = date1.getTime();
+    const date2FixedDifference = date2.getTime();
+    const datesDifference = (date1FixedDifference - date2FixedDifference)/1000;
+    return datesDifference;
+  }
+
 }
 
 export {Age};
