d12cd4fa3dcf159e55f822e2056cfb5998f1e963
Kayl Eubanks <kayleubanks@gmail.com>
2018-04-20 10:03:50 -0700

    Change comparison value for confirming correct conversion. Previous value was too precise testing

--- spec/age_spec.js
+++ spec/age_spec.js
@@ -5,7 +5,7 @@ describe('Conversion of Earth age to other relative ages', function() {
   it('should convert a provided age(in years) to seconds', function(){
     const userAge = 24;
     const ageCaller = new Age();
-    expect(ageCaller.secondConverter(userAge)).toEqual(757366848);
+    expect(ageCaller.secondConverter(userAge)).toEqual(756864000);
   });
 
 });
