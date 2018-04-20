2f16de027c9ce73d2e41de8056cb9a56d741704e
Kayl Eubanks <kayleubanks@gmail.com>
2018-04-20 10:43:20 -0700

    small refactor to DRY code to make ageCaller variable universally available

--- spec/age_spec.js
+++ spec/age_spec.js
@@ -2,17 +2,21 @@ import {Age} from "./../src/age.js";
 
 describe('Conversion of Earth age to other relative ages', function() {
 
-  it('should convert a provided age(in years) to seconds', function(){
+  const ageCaller = new Age;
+
+  it('should convert a provided age in years to seconds', function(){
     const userAge = 24;
-    const ageCaller = new Age();
     expect(ageCaller.secondConverter(userAge)).toEqual(756864000);
   });
 
   it('should determine difference in seconds between two dates', function(){
     const currentDate = new Date(2018, 3, 20);
     const userBirth = new Date(2015, 3, 20);
-    const ageCaller = new Age();
     expect(ageCaller.dateDifference(currentDate, userBirth)).toEqual(94694400);
+  });
+
+  it('should return age in Mercury solar years', function() {
+
   });
 
 });
