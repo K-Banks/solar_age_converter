14471d0e29adf5ef220d4ab3c35a4b2ed1e8ee9e
Kayl Eubanks <kayleubanks@gmail.com>
2018-04-20 14:54:02 -0700

    Allow user to submit information and construct object with information

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {User} from './user.js';
+import {Age} from './age.js';
+
+$(document).ready(function() {
+  $("form#userSubmit").submit(function(event) {
+    event.preventDefault();
+    let userName = $("input#userName").val();
+    let userYear = $("input#userYear").val();
+    let userMonth = $("select#userMonth").val();
+    let userDay = $("input#userDay").val();
+    let newUser = new User(userName, userYear, userMonth, userDay);
+  });
+});
