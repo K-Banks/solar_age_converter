3384356c8fbdc65df3742527cd1f22eeb90bbdbf
Kayl Eubanks <kayleubanks@gmail.com>
2018-04-20 11:40:05 -0700

    add spec test 5, currently failing

--- spec/age_spec.js
+++ spec/age_spec.js
@@ -29,13 +29,11 @@ describe('Conversion of Earth age to other relative ages', function() {
     expect(ageCaller.planetaryAge(earthAgeSeconds, convertName)).toEqual(14.89);
   });
 
+  it('should return age in Mars solar years', function() {
+    const userBirth = new Date(1994, 3, 20);
+    const convertName = 'Mars';
+    let earthAgeSeconds = ageCaller.dateDifference(currentDate, userBirth);
+    expect(ageCaller.planetaryAge(earthAgeSeconds, convertName)).toEqual(45.12);
+  });
 
 });
-
-
-// it('should return age in Mars solar years', function() {
-//   const userBirth = new Date(1994, 3, 20);
-//   const marsConversionFactor = 1.88;
-//   let earthAgeSeconds = ageCaller.dateDifference(currentDate, userBirth);
-//   expect(ageCaller.planetaryAge(earthAgeSeconds, marsConversionFactor)).toEqual(14.89);
-// });
